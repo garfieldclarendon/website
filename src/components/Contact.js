61ef933c3426e1ddef087c01c90a81c9d688720b
Ryan Balla <ryan@Ryans-MBP.attlocal.net>
2021-02-05 18:45:12 -0600

    Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,55 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Contact = (props) => (
-    <section id="contact">
-        <div className="inner">
-            <section>
-                <form method="post" name="contact" data-netlify="true">
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
-                    </div>
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
-                </form>
-            </section>
-            <section className="split">
+const Contact = (props) => {
+    const [isSuccessful, setIsSuccessful] = useState(false);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        let myForm = document.getElementById('contactForm');
+        let formData = new FormData(myForm)
+        fetch('/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString()
+        }).then(() => setIsSuccessful(true)).catch((error) =>
+            setIsSuccessful(false));
+    }
+
+    return (
+        <section id="contact">
+            <div className="inner">
                 <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
-                        <a href="#">yardmaster@garfieldcentral.org</a>
-                    </div>
+                    {isSuccessful && <p>Form submitted, thanks!</p>}
+                    <form action="#" method="post" id="contactForm" name="contact" data-netlify="true" onSubmit={handleSubmit}>
+                        <div className="field half first">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div className="field half">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" rows="6"></textarea>
+                        </div>
+                        <ul className="actions">
+                            <li><input type="submit" value="Send Message" className="special" /></li>
+                            <li><input type="reset" value="Clear" /></li>
+                        </ul>
+                    </form>
                 </section>
-                {/* <section>
+                <section className="split">
+                    <section>
+                        <div className="contact-method">
+                            <span className="icon alt fa-envelope"></span>
+                            <h3>Email</h3>
+                            <a href="#">yardmaster@garfieldcentral.org</a>
+                        </div>
+                    </section>
+                    {/* <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
                         <span>(000) 000-0000 x12387</span>
                     </div>
                 </section> */}
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>4501 N. Clarendon Ave., Basement<br />
+                    <section>
+                        <div className="contact-method">
+                            <span className="icon alt fa-home"></span>
+                            <h3>Address</h3>
+                            <span>4501 N. Clarendon Ave., Basement<br />
                         Chicago, IL 60640<br />
                         United States of America</span>
-                    </div>
+                        </div>
+                    </section>
                 </section>
-            </section>
-        </div>
-    </section>
-)
+            </div>
+        </section>
+    )
+}
 
 export default Contact
