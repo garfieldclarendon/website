4ec4f6724b712bba436706f3806abaa002809282
Ryan Balla <ryanballa@Ryans-MacBook-Air.local>
2024-03-18 15:35:58 -0500

    Adding endpoint

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,8 @@ module.exports = {
           {
             resolve: 'gatsby-plugin-mailchimp',
             options: {
+              endpoint:
+                'https://garfieldcentral.us9.list-manage.com/subscribe/post?u=38fd98b265c3898b09b9cdc49&amp;id=3098b8015e&amp;f_id=00bac1e0f0', // string; add your MC list endpoint here; see instructions below
               timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
             },
           },
