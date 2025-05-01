51d2c78f439c0a7ec78b4ae5a52a62ef3a399815
Ryan Balla <ryan@ryanballa.com>
2025-05-01 15:32:29 -0500

    Adding Donation Link

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,28 +3,78 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
 const Menu = (props) => (
-    <nav id="menu">
-        <div className="inner">
-            <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/blog">Blog</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/membership">Membership</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/outreach">Outreach</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/tour">Tour</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/videos">Videos</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/social-media">Connect</Link></li>
-            </ul>
-            <ul className="actions vertical">
-                <li><a href="/visit" className="button special fit">Visit</a></li>
-                <li><a onClick={e => { e.preventDefault(); window.open('https://locomotivehouse.com', '_blank'); }} href="https://locomotivehouse.com" className="button fit">Member Log In</a></li>
-            </ul>
-        </div>
-        <button className="close" onClick={props.onToggleMenu}>Close</button>
-    </nav>
+  <nav id="menu">
+    <div className="inner">
+      <ul className="links">
+        <li>
+          <Link onClick={props.onToggleMenu} to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/donate">
+            Donate
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/blog">
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/membership">
+            Membership
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/outreach">
+            Outreach
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/tour">
+            Tour
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/videos">
+            Videos
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/social-media">
+            Connect
+          </Link>
+        </li>
+      </ul>
+      <ul className="actions vertical">
+        <li>
+          <a href="/visit" className="button special fit">
+            Visit
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={(e) => {
+              e.preventDefault()
+              window.open('https://locomotivehouse.com', '_blank')
+            }}
+            href="https://locomotivehouse.com"
+            className="button fit"
+          >
+            Member Log In
+          </a>
+        </li>
+      </ul>
+    </div>
+    <button className="close" onClick={props.onToggleMenu}>
+      Close
+    </button>
+  </nav>
 )
 
 Menu.propTypes = {
-    onToggleMenu: PropTypes.func
+  onToggleMenu: PropTypes.func,
 }
 
 export default Menu
